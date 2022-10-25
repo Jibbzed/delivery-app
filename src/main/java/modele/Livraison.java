543b082bda7274991048db4ba7ded7334b3ca159
ClementINSALYON <clement.bousseau@insa-lyon.fr>
2022-10-25 11:04:22 +0200

    Creation Tournee

--- src/main/java/modele/Livraison.java
+++ src/main/java/modele/Livraison.java
@@ -4,13 +4,48 @@ import java.util.List;
 
 public class Livraison {
 
-    public Intersection lieuLivraison;
+
+    public Intersection destinationLivraison;
     // public Coursier coursierLivraison;
-    public List<Troncon> tronconLiesLivraison ; // liste de troncons menant à cette livraison, nulle au début puis remplie ??
+    public List<Troncon> parcoursLivraison; // parcours à faire pour la livraison
+    public Intersection origineLivraison; // première intersection du parcours de la livraison
     public int fenetreHoraireLivr;
 
-    public Livraison(Intersection lieuLivraison, int fenetreHoraireLivr) {
-        this.lieuLivraison = lieuLivraison;
+    public Livraison(Intersection destinationLivraison, int fenetreHoraireLivr) {
+        this.destinationLivraison = destinationLivraison;
         this.fenetreHoraireLivr = fenetreHoraireLivr;
     }
+
+    public Intersection getOrigineLivraison() {
+        return origineLivraison;
+    }
+
+    public void setOrigineLivraison(Intersection origineLivraison) {
+        this.origineLivraison = origineLivraison;
+    }
+
+    public List<Troncon> getParcoursLivraison() {
+        return parcoursLivraison;
+    }
+
+    public void setParcoursLivraison(List<Troncon> parcoursLivraison) {
+        this.parcoursLivraison = parcoursLivraison;
+    }
+
+    public int getFenetreHoraireLivr() {
+        return fenetreHoraireLivr;
+    }
+
+    public void setFenetreHoraireLivr(int fenetreHoraireLivr) {
+        this.fenetreHoraireLivr = fenetreHoraireLivr;
+    }
+
+    public Intersection getDestinationLivraison() {
+        return destinationLivraison;
+    }
+
+    public void setDestinationLivraison(Intersection destinationLivraison) {
+        this.destinationLivraison = destinationLivraison;
+    }
+
 }
---
+++ src/main/java/modele/Tournee.java
@@ -0,0 +1,25 @@
+package modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tournee {
+
+    public List<Livraison> livraisons = new ArrayList<>();
+
+    public Tournee(List<Livraison> livraisons) {
+        this.livraisons = livraisons;
+    }
+
+    public List<Livraison> getLivraisons() {
+        return livraisons;
+    }
+
+    public void ajouterLivraison(Livraison l){
+        this.livraisons.add(l);
+    }
+
+    public void retirerLivraison(Livraison l){
+        this.livraisons.remove(l);
+    }
+}
