36fe4bdc347a12a629cabed31ae6a0c81db9dcb6
abelleminm <bellemin.alex@gmail.com>
2022-10-25 10:54:58 +0200

    Test coursier + modifs coursier

--- src/main/java/modele/Coursier.java
+++ src/main/java/modele/Coursier.java
@@ -8,16 +8,20 @@ public class Coursier {
     private final String prenom;
     private boolean planifie;
 
+    private boolean valide;
+
     public Coursier(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
         this.planifie = false;
+        this.valide = true;
     }
 
     public Coursier(String nom, String prenom, boolean planifie) {
         this.nom = nom;
         this.prenom = prenom;
         this.planifie = planifie;
+        this.valide = true;
     }
 
     public String getNom() {
@@ -37,22 +41,35 @@ public class Coursier {
     }
 
     /**
-     * Réfère null à un coursier, afin de ne plus l'utiliser
-     * @param c : coursier à supprimer
+     * Correspond à un set de valide, mais plus explicite
      */
-    public void deleteCoursier(Coursier c) {
-        c = null;
+    public void deleteCoursier() {
+        this.valide = false;
     }
 
     /**
      * Crée une fausse base de donnée de coursiers
+     * @param nombre : nombre de coursier à créer
      */
-    public static void initCoursier() {
+    public static Coursier[] initCoursier(int nombre) {
         Faker faker = new Faker();
-        Coursier c1 = new Coursier(faker.name().lastName(), faker.name().firstName());
-        Coursier c2 = new Coursier(faker.name().lastName(), faker.name().firstName());
-        Coursier c3 = new Coursier(faker.name().lastName(), faker.name().firstName());
-        Coursier c4 = new Coursier(faker.name().lastName(), faker.name().firstName());
-        Coursier c5 = new Coursier(faker.name().lastName(), faker.name().firstName());
+        Coursier[] coursierArray = new Coursier[nombre];
+        for (int i = 0 ; i < nombre ; i++) {
+            coursierArray[i] = new Coursier(faker.name().lastName(), faker.name().firstName());
+        }
+        return coursierArray;
+    }
+
+    @Override
+    public String toString() {
+        if (this.valide) {
+            return "Coursier{" +
+                    "nom='" + nom + '\'' +
+                    ", prenom='" + prenom + '\'' +
+                    ", planifie=" + planifie +
+                    '}';
+        } else {
+            return "Coursier supprimé.";
+        }
     }
 }
