34086423d3162f473d2abf0ac282869f0cf4b4a8
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2022-10-26 02:16:35 +0200

    afficher une map initialement sur lyon, configuration de biblio, mais la carte n'est pas bien centree

--- src/main/java/vue/App.java
+++ src/main/java/vue/App.java
@@ -1,29 +1,50 @@
 package vue;
 
+import com.sothawo.mapjfx.Projection;
+import controleur.Controller;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    /** Logger for the class */
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        launch();
+        logger.trace("begin main");
+        launch(args);
+        logger.trace("end main");
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        logger.info("starting DemoApp");
+        String fxmlFile = "/vue/DemoApp.fxml";
+        logger.debug("loading fxml file {}", fxmlFile);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
+        logger.trace("stage loaded");
+
+        final Controller controller = fxmlLoader.getController();
+        final Projection projection = getParameters().getUnnamed().contains("wgs84")
+                ? Projection.WGS_84 : Projection.WEB_MERCATOR;
+        controller.initMapAndControls(projection);
+
+        Scene scene = new Scene(rootNode);
+        logger.trace("scene created");
+
+        primaryStage.setTitle("sothawo mapjfx demo application");
+        primaryStage.setScene(scene);
+        logger.trace("showing scene");
+        primaryStage.show();
+
+        logger.debug("application start method finished.");
+    }
 }
